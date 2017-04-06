3a61a258ad6b5204f29af58b41dc3b983fe3f521
Bryan Ortiz <ortiz.b.t@gmail.com>
2017-04-06 13:30:56 -0500

    Added initial view controller and temp added manager for accessing raw JSON

--- Axxess Challenge/Axxess Challenge/AppDelegate.swift
+++ Axxess Challenge/Axxess Challenge/AppDelegate.swift
@@ -8,14 +8,21 @@
 
 import UIKit
 
+import SwiftyJSON
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let vc = ButtonViewController()
+        window?.rootViewController = vc
+        
+        let json = AxxessAlamofire.sharedInstance.getJSONV2()
+        print(json())
         return true
     }
 
